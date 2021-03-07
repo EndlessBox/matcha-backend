42d5ff51e230a76abab20fa1feb7ed7773dfd342
msidqi <mouadmr89@gmail.com>
2021-03-07 15:23:36 +0100

    feat: fix: getUserMatches result

--- models/match.js
+++ models/match.js
@@ -1,77 +1,86 @@
-var dbConnection = require('./dbConnection')().getDb();
-
-
-
+var dbConnection = require("./dbConnection")().getDb();
 
 module.exports = class matchModel {
+  constructor() {}
 
-    constructor(){}
+  createMatch(match) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        match["id"] = null;
+        match["dateOfMatch"] = new Date();
+        let [results, _] = await dbConnection.query(
+          {
+            sql: "INSERT INTO `match` SET ?",
+            timeout: 40000,
+          },
+          match
+        );
+        resolve(results.insertedId);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
 
-    createMatch (match) {
-        return new Promise(async (resolve, reject) => {
-            try {
-                match['id'] = null;
-                match['dateOfMatch'] = new Date();
-                let [results, _] = await dbConnection.query({
-                    sql: "INSERT INTO `match` SET ?",
-                    timeout: 40000
-                },
-                match)
-                resolve(results.insertedId);
-            } catch (error) {
-                reject(error);
-            }
-        })
-    }
+  getMatchedHistoryByUserId(userId) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let [results, _] = await dbConnection.query(
+          {
+            sql:
+              "select u.userName, u.firstName, u.lastName, m.dateOfMatch from user u inner JOIN match m on u.id=m.matcher where m.matched=?",
+          },
+          userId
+        );
+        resolve(results);
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
 
+  deleteMatch(matcherId, matchedId) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let [results, _] = await dbConnection.query(
+          {
+            sql: "DELETE FROM `match` m WHERE m.matcher=? AND m.matched=?",
+          },
+          [matcherId, matchedId]
+        );
 
+        resolve(results.affectedRows);
+      } catch (error) {
+        console.log(error);
+        reject(error);
+      }
+    });
+  }
 
-    getMatchedHistoryByUserId(userId) {
-        return new Promise(async (resolve, reject) => {
-            try {
-                
-                let [results, _] = await dbConnection.query({
-                    sql: "select u.userName, u.firstName, u.lastName, m.dateOfMatch from user u inner JOIN match m on u.id=m.matcher where m.matched=?"
-                }, userId);
-                resolve(results);
-            } catch (error) {
-                reject(error);
-            }
-        })
-    }
+  getUserMatches(userId) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let [results, _] = await dbConnection.query(
+          {
+            sql:
+              "SELECT u.id, u.userName, g.gender,o.orientation, u.experience FROM `user` u INNER JOIN `gender` g ON u.genderId=g.id INNER JOIN `sexualOrientation` o ON u.orientationId=o.id INNER JOIN `match` m ON u.id=m.matcher WHERE m.matched=?",
+          },
+          userId
+        );
 
-    deleteMatch(matcherId, matchedId) {
-        return new Promise(async (resolve, reject) => {
-            try {
-                let [results, _] = await dbConnection.query({
-                    sql: "DELETE FROM `match` m WHERE m.matcher=? AND m.matched=?"
-                }, [matcherId, matchedId]);
-
-                resolve(results.affectedRows);
-            } catch (error) {
-                console.log(error)
-                reject(error)
-            }
-        })
-    }
-
-    getUserMatches(userId) {
-        return new Promise (async (resolve, reject) => {
-            try {
-                let [results, _] = await dbConnection.query({
-                    sql: "SELECT u.id, u.userName, g.gender,o.orientation, u.experience FROM `user` u INNER JOIN `gender` g ON u.genderId=g.id INNER JOIN `sexualOrientation` o ON u.orientationId=o.id INNER JOIN `match` m ON u.id=m.matcher WHERE m.matched=?"
-                }, userId);        
-                
-                let [results2, _2] = await dbConnection.query({
-                    sql: "SELECT u.id, u.userName, g.gender,o.orientation, u.experience FROM `user` u INNER JOIN `gender` g ON u.genderId=g.id INNER JOIN `sexualOrientation` o ON u.orientationId=o.id INNER JOIN `match` m ON u.id=m.matched WHERE m.matcher=?"
-                }, userId);
-                
-                let result = [results[0], results2[0]]
-                resolve(result);
-            } catch (error) {
-                console.log(error)
-                reject(error);
-            }
-        })
-    }
-}
+        let [results2, _2] = await dbConnection.query(
+          {
+            sql:
+              "SELECT u.id, u.userName, g.gender,o.orientation, u.experience FROM `user` u INNER JOIN `gender` g ON u.genderId=g.id INNER JOIN `sexualOrientation` o ON u.orientationId=o.id INNER JOIN `match` m ON u.id=m.matched WHERE m.matcher=?",
+          },
+          userId
+        );
+        let result = [...results, ...results2];
+        resolve(result);
+      } catch (error) {
+        console.log(error);
+        reject(error);
+      }
+    });
+  }
+};
--- services/matchService.js
+++ services/matchService.js
@@ -1,97 +1,104 @@
-let MatchModel = require('../models/match');
-let UserModel = require('../models/user');
-let rankService = require('./rankService');
-var xpConfig = require('../config/config').Experience;
+let MatchModel = require("../models/match");
+let UserModel = require("../models/user");
+let rankService = require("./rankService");
+var xpConfig = require("../config/config").Experience;
 var cacheService = new (require("./cacheService"))();
 
 module.exports = class matchService {
-    constructor(){}
+  constructor() {}
 
-    createMatch (query, user) {
-        return new Promise(async (resolve, reject) => {
-            try{
+  createMatch(query, user) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let matchModel = new MatchModel();
+        let userModel = new UserModel();
+        let rankServ = new rankService();
 
-                let matchModel = new MatchModel();
-                let userModel = new UserModel();
-                let rankServ = new rankService();
-                
-                if (user.userName !== query.matcher)
-                    return reject({message: "Matcher from outer source.", status: 403});
+        if (user.userName !== query.matcher)
+          return reject({ message: "Matcher from outer source.", status: 403 });
 
-                let matcher = await userModel.getUserByAttribute('userName', query.matcher);
-                let matched = await userModel.getUserByAttribute('userName', query.matched);
+        let matcher = await userModel.getUserByAttribute(
+          "userName",
+          query.matcher
+        );
+        let matched = await userModel.getUserByAttribute(
+          "userName",
+          query.matched
+        );
 
-                await matchModel.createMatch({matcher: matcher.id, matched: matched.id});
+        await matchModel.createMatch({
+          matcher: matcher.id,
+          matched: matched.id,
+        });
 
-                resolve(true);
-            } catch(err) {
-                if (err.code === 'ER_DUP_ENTRY' && err.errno == 1062)
-                {
-                    console.error(new Date().toLocaleDateString(),
-                    err.sqlMessage,
-                    "-- UserName: " + user.userName);
-                    return resolve({message: "Nothing changed"});
-                }
-                reject(err);
-            }
-        })
-    }
+        resolve(true);
+      } catch (err) {
+        if (err.code === "ER_DUP_ENTRY" && err.errno == 1062) {
+          console.error(
+            new Date().toLocaleDateString(),
+            err.sqlMessage,
+            "-- UserName: " + user.userName
+          );
+          return resolve({ message: "Nothing changed" });
+        }
+        reject(err);
+      }
+    });
+  }
 
+  deleteMatch(query, user) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let matchModel = new MatchModel();
+        let userModel = new UserModel();
 
+        let matched = await userModel.getUserByAttribute(
+          "userName",
+          query.matched
+        );
+        let matcher = user;
 
-    deleteMatch (query, user) {
-        return new Promise(async(resolve, reject) => {
-            try {
-                
-                let matchModel = new MatchModel();
-                let userModel = new UserModel();
+        await matchModel.deleteMatch(matcher.id, matched.id);
+        await matchModel.deleteMatch(matched.id, matcher.id);
 
-                let matched = await userModel.getUserByAttribute("userName", query.matched);
-                let matcher = user;
+        resolve({ message: "Unmatched succefuly.", status: "200" });
+      } catch (error) {
+        reject(error);
+      }
+    });
+  }
 
-                await matchModel.deleteMatch(matcher.id, matched.id);
-                await matchModel.deleteMatch(matched.id, matcher.id);
+  getUserConnectedMatchesSocketsId(userId) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let matchModel = new MatchModel();
+        let userMatches = await matchModel.getUserMatches(userId);
 
-                resolve({message: "Unmatched succefuly.", status: "200"});
+        if (!userMatches.length) return resolve([]);
 
-            } catch (error) {
-                reject(error)
-            }
-        })
-    }
+        let result = userMatches.map(async (match) => {
+          let notifiedSocket = await cacheService.getUserSocketId(
+            match.matcher === userId ? match.matched : match.matcher
+          );
+          if (notifiedSocket) return notifiedSocket;
+        });
 
-    getUserConnectedMatchesSocketsId (userId) {
-        return new Promise(async(resolve, reject) => {
-            try {
+        resolve(Promise.all(result));
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
 
-                let matchModel = new MatchModel();
-                let userMatches = await matchModel.getUserMatches(userId);
-                
-                if (!userMatches.length)
-                    return resolve([]);
-
-                let result = userMatches.map(async match => {
-                    let notifiedSocket = await cacheService.getUserSocketId(match.matcher === userId ? match.matched : match.matcher);
-                    if (notifiedSocket)
-                        return (notifiedSocket)
-                })
-
-                resolve(Promise.all(result));
-            } catch(err) {
-                reject(err);
-            }
-        })
-    }
-
-    getUserMatches(userId){
-        return new Promise(async (resolve, reject) => {
-            try{
-                let matchModel = new MatchModel();
-                let result = await matchModel.getUserMatches(userId);
-                resolve(result);
-            } catch(err) {
-                reject(err);
-            }
-        })
-    }
-}
+  getUserMatches(userId) {
+    return new Promise(async (resolve, reject) => {
+      try {
+        let matchModel = new MatchModel();
+        let result = await matchModel.getUserMatches(userId);
+        resolve(result);
+      } catch (err) {
+        reject(err);
+      }
+    });
+  }
+};
